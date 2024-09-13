73982d2ce8afe5a469680ebacd5c8961628d2da6
Victor Granados <victorgranadosp@gmail.com>
2024-09-13 22:02:29 +0200

    Almost done with spectral density plots on direct diagonalization.

--- DisorderedSystems/src/ProjectPlots/spectral_density.jl
+++ DisorderedSystems/src/ProjectPlots/spectral_density.jl
@@ -1,20 +1,77 @@
-function plotSpectralDensity(λ_series; plot_wigner::Bool = false)
+function plotSpectralDensity(λ_series; 
+    bins::Int=50, histo_lw::Union{Float64, Int} = 1.7, colors=nothing, labels=nothing,
+    plot_wigner::Bool = false, kw=nothing)
 
+    minima = [minimum(vec(λ_samples)) for λ_samples in λ_series]
+    maxima = [maximum(vec(λ_samples)) for λ_samples in λ_series]
+    min_λ  = minimum(minima)
+    max_λ = maximum(maxima)
 
-    bins = 50
-    bin_edges = range(-3, 3, length=bins+1)
+    bin_step = (max_λ - floor(min_λ, digits=4)) / bins
 
-    histo = scatterhist(vec(λ_series[1]), normalize=:pdf, bins=bin_edges, color=:red)
-    scatterhist!(vec(λ_series[2]), normalize=:pdf, bins=bin_edges, color=:orange)
+    if isnothing(labels)
+        labels = [nothing for _ in 1:(size(λ_series)[1] + 1)]
+    end
+
+    if isnothing(colors)
+        # Add one extra color for the plot of Wigner's semicircle law.
+        colors = [k for k in 1:(size(λ_series)[1] + 1)]
+    end
+    
+    histo = nothing
+    
+    for (k, (λ_samples, label, color)) in enumerate(zip(λ_series, labels, colors))
+        min_idx = floor(Int, (minima[k] - min_λ) / bin_step)
+        max_idx = ceil(Int, (maxima[k] - min_λ) / bin_step)
+        min_edge = min_λ + min_idx * bin_step
+        bin_edges = range(min_edge, step=bin_step, length=max_idx - min_idx + 1)
+
+        if k == 1
+            histo = stephist(
+                vec(λ_samples), normalize=:pdf, bins=bin_edges, lw=histo_lw, 
+                color=color, label=label, legendtitle=labels[end])
+        else
+         stephist!(
+            vec(λ_samples), normalize=:pdf, bins=bin_edges, lw=histo_lw, 
+            color=color, label=label)
+        end
+    end
 
     if plot_wigner
         λ_range = range(-2., 2., length=500)
         plot!(
             λ_range, sqrt.(4 .- λ_range.^2)/(2*π), 
-            lc=:blue, lw=2.4, label="Wigner's law", z_order=1)
+            lc=colors[end], lw=2.5, label="\$ \\mathrm{Wigner's} \\;\\; \\mathrm{law} \$", z_order=1)
     end
 
-    display(histo)
+    # x-axis ticks.
+    xticks_max = round(Int, max(abs(min_λ), max_λ))
+    xticks_range = -xticks_max:xticks_max
+    xticks_label = ["\$ $(i) \$" for i ∈ xticks_range]
+    plot!(xticks=(xticks_range, xticks_label))
 
-    return
+    # y-axis ticks
+    bar_heights = [y for y in histo.series_list[1].plotattributes[:y] if !isnan(y)]
+    y_max = maximum(bar_heights)
+    yticks_range = [y for y in 0:0.1:1 if y <= 1.05*y_max]
+    
+    # Avoid saturating with many ticks.
+
+    max_yticks = 6
+
+    if length(yticks_range) > max_yticks
+        yticks_max = maximum(yticks_range)
+        yticks_range = range(0, yticks_max, length=max_yticks)
+    end
+    
+    plot!(yminorticks=2)
+    
+    yticks_label = ["\$ $(round(y, digits=2)) \$" for y in yticks_range]
+    plot!(yticks=(yticks_range, yticks_label))
+
+    if !isnothing(kw)
+        plot!(;kw...)
+    end
+
+    return histo
 end
---
+++ exercises/exercise2.jl
@@ -0,0 +1,137 @@
+using Random
+
+using DisorderedSystems: saveData2h5, loadDataFromH5
+using DisorderedSystems.RandomMatrixTheory: sampleEigenvalues
+using DisorderedSystems.ProjectPlots: plotSpectralDensity
+
+
+"""
+    getDefaultValues(point::Int)
+
+Retrieve the parameter values according to each part of exercise 2 part 1.
+
+Convenience function to avoid cluttering the main function. The points 1 to 4
+correspond to the following parameter values:
+1. 10 samples of linear size 2^10 with mean connectivity 3.
+2. 10 samples of linear size 2^10 with mean connectivity N - 1.
+3. 1 sample of linear size 2^12 with mean connectivity N - 1.
+4. 1 sample of linear size 2^12 for mean connectivity values of 50, 100 and 200.
+"""
+function getDefaultValues(point::Int8)
+
+    !(point in (1, 2, 3, 4)) && error("Part number not valid. Options are 1 - 4")
+
+    if point <= 2
+        num_samples = 10
+        linear_size = 2^10
+        avg_connect = (point == 1) ? 3 : linear_size - 1
+    else
+        num_samples = 1
+        linear_size = 2^12
+        avg_connect = (point == 3) ? linear_size - 1 : [20]
+    end
+
+    return linear_size, avg_connect, num_samples
+end
+
+
+function getLambdaSeries(plot_num::Int8; save_data::Bool=false, load_data::Bool=false)
+
+    !(plot_num in [1, 2]) && error("Plot number not valid. Options are only 1 or 2.")
+
+    points = (plot_num == 1) ? Int8[1, 2] : Int8[3, 4]
+
+    data_dir = "$(splitdir(@__DIR__)[1])" * "/data/"  # Path to data/
+    filename = "exercise2.h5"
+    filepath = data_dir * filename
+    group_nm = "eigenvalues"  # Data group name within the file.
+
+    λ_series = []
+    labels = []
+
+    for point in points
+        N, c_values, num_samples = getDefaultValues(point)
+
+        for c in c_values
+            data_name = "size_$(N)_c_$(c)_samples_$(num_samples)"
+
+            if load_data
+                λ_samples = loadDataFromH5(filepath, data_name; group_name=group_nm)
+            else
+                println("Generating $(num_samples) samples of" * 
+                    " size N=$(N) and mean connectivity c=$(c)...")
+                # Reseed at each value of 'c', so the order of sampling is irrelevant.
+                Random.seed!(42)  
+                λ_samples = sampleEigenvalues(N, c, num_samples)
+                
+                if save_data
+                    println("  Saving data to $(filepath)")
+                    saveData2h5(
+                        filepath, data_name, λ_samples; 
+                        group_name=group_nm, overwrite=true)
+                end
+            end
+            push!(λ_series, λ_samples)
+            push!(labels, "\$ c = $(c) \$")
+        end
+    end
+
+    return λ_series, labels
+end
+
+function main()
+    plot_num::Int8 = 2
+    load_data = true
+    save_data = true  # Ignored if values are loaded.
+    # save_figure = false
+
+    # 1.1 Generate 10 samples with N=2^10, c=3, and estimate ρ(λ).
+    # 1.2 Generate 10 samples with N=2^10, c=N-1, and estimate ρ(λ). 
+    # 1.3 Generate 1 sample with N=2^12, c=N-1, estimate ρ(λ) and 
+    #     compare with Wigner's semicircle law.
+    # 1.4 Compare spectral density histograms for different values
+    #     of c, to Wigner's semicircle law.
+    
+    λ_series, labels = getLambdaSeries(plot_num; load_data=load_data, save_data=save_data)
+
+    kw = (;
+        xlabel="\$ \\lambda \$",
+        xminorgrid=true,
+        xminorticks=2,
+        xlabelfontsize=18,
+
+        ylabel="\$ \\rho(\\lambda) \$",
+        yminorgrid=true,
+        yminorticks=2,
+        ylabelfontsize=18,
+
+        tickfontsize=18,
+        legendtitlefontsize=16,
+        legendfontsize=15,
+        legend=:bottom,
+
+        framestyle=:box,
+        size=(600, 400),
+        dpi=400
+        )
+
+    if plot_num == 1
+        push!(labels, "\$ N = $(2^10) \$")
+        colors = [:blue, :red]
+        plot_wigner = false
+    else
+        push!(labels, "\$ N = $(2^12) \$")
+        colors = [:blue, :forestgreen, :red]
+        plot_wigner = true
+    end
+
+    histogram = plotSpectralDensity(
+        λ_series, plot_wigner=plot_wigner, colors=colors, labels=labels, kw=kw, bins=80)
+
+    display(histogram)
+
+    return nothing
+end
+
+
+main()
