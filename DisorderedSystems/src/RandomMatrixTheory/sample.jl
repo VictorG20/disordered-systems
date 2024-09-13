069b049d54df9f09e0f61de9b869b83a11e09c70
Victor Granados <victorgranadosp@gmail.com>
2024-09-13 10:54:00 +0200

    Added option to return as sparse matrix.

--- DisorderedSystems/src/RandomMatrixTheory/sample.jl
+++ DisorderedSystems/src/RandomMatrixTheory/sample.jl
@@ -1,17 +1,30 @@
 """
-    sampleGaussianWignerMatrix(N::Int, c::Int; μ::Real = 0, σ::Real = 1/sqrt(c))
+    sampleGaussianWignerMatrix(linear_size::Int, mean_connectivity::Int; 
+    mean::Real = 0, std::Real = 1/sqrt(mean_connectivity), sparse::Bool = false)
 
-Sample a (real) Gaussian Wigner Matrix of size N x N, mean connectivity c.
-The Gaussian distribution has by default a mean μ = 0 and a standard deviation
-of σ = 1/sqrt(c).
+Sample a real Gaussian Wigner Matrix.
+
+The matrix is generated from the element-wise product of the adjacency matrix of 
+a random regular graph of the given size and mean connectivity, with a symmetric
+matrix whose entries are sampled from a normal distribution, defined by the mean
+and standard deviation.
+
+# Keyword Arguments
+- `mean::Real=0`: Mean of the Gaussian distribution.
+- `std::Real=1/\\sqrt{mean_connectivity}`: Standard deviation of the Gaussian distribution.
+- `sparse::Bool=false`: If true, return the matrix as type `SparseMatrixCSC`.
 """
-function sampleGaussianWignerMatrix(N::Int, c::Int; μ::Real = 0, σ::Real = 1/sqrt(c))
+function sampleGaussianWignerMatrix(linear_size::Int, mean_connectivity::Int; 
+    mean::Real = 0, std::Real = 1/sqrt(mean_connectivity), sparse::Bool = false)
 
-    rrg = random_regular_graph(N, c)
+    rrg = random_regular_graph(linear_size, mean_connectivity)
     A = adjacency_matrix(rrg)
-    weights_distro = Normal(μ, σ)
-    J = Symmetric(rand(weights_distro, (N, N)))
+    weights_distro = Normal(mean, std)
+    J = Symmetric(rand(weights_distro, (linear_size, linear_size)))
     M = A .* J
-    M = M .* A  # This passes the matrix as a sparse matrix.
+
+    # If not sparse, return as a full, symmetric, matrix.
+    M = sparse ? (M .* A) : Symmetric(M)
+
     return M
 end
