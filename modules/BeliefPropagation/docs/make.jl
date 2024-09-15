6efd261788bd5840aa68604e97f7133bf81808ed
Victor Granados <victorgranadosp@gmail.com>
2024-09-15 19:38:44 +0200

    Added deploydocs to make.jl

--- modules/BeliefPropagation/docs/make.jl
+++ modules/BeliefPropagation/docs/make.jl
@@ -2,3 +2,9 @@ using Documenter
 using BeliefPropagation
 
 makedocs(sitename="BeliefPropagation")
+
+deploydocs(; repo = "github.com/VictorG20/disordered-systems.git",
+             dirname="BeliefPropagation",
+             tag_prefix="modules/BeliefPropagation-",
+             # ...any additional kwargs
+             )
